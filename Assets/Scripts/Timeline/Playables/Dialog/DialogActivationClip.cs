4986746a62b1de526d906362e4628843064de295
Mrinmoy Kalita <mrin443@gmail.com>
2025-06-11 22:14:25 +0530

    [ADD]: Objective intro cutscene

--- Assets/Scripts/Timeline/Playables/Dialog/DialogActivationClip.cs
+++ Assets/Scripts/Timeline/Playables/Dialog/DialogActivationClip.cs
@@ -6,11 +6,12 @@ namespace Timeline.Playables.Dialog
 {
     public class DialogActivationClip : PlayableAsset
     {
+        [SerializeField] private string author;
         [SerializeField, ResizableTextArea] private string promptText;
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
             ScriptPlayable<DialogBehaviour> _graph = ScriptPlayable<DialogBehaviour>.Create(graph);
-            Debug.Log(nameof(CreatePlayable));
+            _graph.GetBehaviour().Author = author;
             _graph.GetBehaviour().Prompt = promptText;
             return _graph;
         }
--- Assets/Scripts/Timeline/Playables/Dialog/DialogBehaviour.cs
+++ Assets/Scripts/Timeline/Playables/Dialog/DialogBehaviour.cs
@@ -1,32 +1,25 @@
 ﻿using Game.Managers;
-using UnityEngine;
 using UnityEngine.Playables;
 
 namespace Timeline.Playables.Dialog
 {
     public class DialogBehaviour : PlayableBehaviour
     {
+        public string Author;
         public string Prompt;
         public override void OnBehaviourPlay(Playable playable, FrameData info)
         {
             base.OnBehaviourPlay(playable, info);
-            Debug.Log($"{nameof(OnBehaviourPlay)} : {Prompt}");
             PushDialog(playable);
         }
 
         private async void PushDialog(Playable playable)
         {
-            _ = DialogHandler.Instance.PushPromptText("Mira: ", Prompt);
+            _ = DialogHandler.Instance.PushPromptText($"{Author}: ", Prompt);
             playable.GetGraph().GetRootPlayable(0).SetSpeed(0);
             await DialogHandler.Instance.WaitForNextAsync("");
             DialogHandler.Instance.ClearDialog();
             playable.GetGraph().GetRootPlayable(0).SetSpeed(1);
-        }
-
-        public override void OnBehaviourPause(Playable playable, FrameData info)
-        {
-            base.OnBehaviourPause(playable, info);
-            Debug.Log(nameof(OnBehaviourPause));
         }
     }
 }
